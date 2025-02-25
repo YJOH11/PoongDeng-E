8e23449a37f751a4e76fe5ba0bf8fef712cc9a44
YJOH11 <yjohmail@naver.com>
2025-02-25 20:13:27 +0900

    test

---
+++ practice/Practice.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("연습중입니다");
+        System.out.println("두번째연습중입니다");
+    }
+
+}
